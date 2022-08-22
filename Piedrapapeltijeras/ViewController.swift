930c71d749d7d93052a6bc42fceeffcfbc7e29a8
Andrea Beltrán Quintana <a01568493@chi-1115-17.svcs.itesm.mx>
2022-08-22 11:58:40 -0600

    first commit

---
+++ Piedrapapeltijeras/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Piedrapapeltijeras
+//
+//  Created by Andrea Beltrán Quintana on 18/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
