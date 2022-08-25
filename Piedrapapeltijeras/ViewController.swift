6511e571c6088ce664fb66b71eb6522b9241a14e
Andrea Beltrán Quintana <a01568493@chi-1115-17.svcs.itesm.mx>
2022-08-24 20:01:54 -0600

    subir documentos

--- Piedrapapeltijeras/ViewController.swift
+++ Piedrapapeltijeras/ViewController.swift
@@ -9,11 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var opc:Int = 0
+    @IBOutlet var btnRock: UIButton!
+    @IBOutlet var btnPaper: UIButton!
+    @IBOutlet var btnScissors: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        btnRock.tag = 1
+        btnPaper.tag = 2
+        btnScissors.tag = 3
     }
 
 
+    @IBAction func clickSelectOption(_ sender: UIButton) {
+        switch sender.tag{
+            case 1:
+                opc = 1
+                break;
+            case 2:
+                opc = 2
+                break;
+            case 3:
+                opc = 3
+                break;
+            default:
+                break;
+        }
+        
+        let controller: ResultController
+        controller = storyboard?.instantiateViewController(identifier: "ResultController") as! ResultController
+        controller.opc = opc
+        present(controller, animated: true, completion: nil)
+    }
 }
-
